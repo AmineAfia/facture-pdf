c5bf816344589b8d38aab83ea035396d5665db74
AmineAfia <afia.amine@gmail.com>
2018-06-21 22:35:54 +0100

    More translation

--- rechnung.php
+++ rechnung.php
@@ -122,52 +122,52 @@ $html .= nl2br($facture_footer);
 
 //////////////////////////// Erzeugung eures PDF Dokuments \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-// TCPDF Library laden
+// importer tcpdf
 require_once('tcpdf/tcpdf.php');
 
-// Erstellung des PDF Dokuments
+// Constriure document PDF
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-// Dokumenteninformationen
+// Infos du document
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor($auteur_pdf);
 $pdf->SetTitle('Facture '.$num_facture);
 $pdf->SetSubject('Facture '.$num_facture);
 
 
-// Header und Footer Informationen
+// Information du Header et Footer
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
-// Auswahl des Font
+// Choix du Font
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-// Auswahl der MArgins
+// Choix du Mrgins
 $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
-// Automatisches Autobreak der Seiten
+// retour au page automatique
 $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
 // Image Scale 
 $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-// Schriftart
+// Font d'ecriture
 $pdf->SetFont('dejavusans', '', 10);
 
-// Neue Seite
+// Nouvelle page
 $pdf->AddPage();
 
-// Fügt den HTML Code in das PDF Dokument ein
+// Inserer le code HTML dans le PDF
 $pdf->writeHTML($html, true, false, true, false, '');
 
-//Ausgabe der PDF
+//Generation du PDF
 
-//Variante 1: PDF direkt an den Benutzer senden:
+//Variante 1: Envoyer PDF aux utilisateur:
 $pdf->Output($pdfName, 'I');
 
-//Variante 2: PDF im Verzeichnis abspeichern:
+//Variante 2: Enregister PDF au serveur:
 //$pdf->Output(dirname(__FILE__).'/'.$pdfName, 'F');
 //echo 'PDF herunterladen: <a href="'.$pdfName.'">'.$pdfName.'</a>';
 
